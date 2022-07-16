4f2c87b0fcd7c41ed5325d65fad56a10cbba78c1
kunminx <kunminx@gmail.com>
2022-07-16 11:17:33 +0800

    simplify requester code

--- app/src/main/kotlin/com/kunminx/purenote/domain/request/NoteRequester.kt
+++ app/src/main/kotlin/com/kunminx/purenote/domain/request/NoteRequester.kt
@@ -20,30 +20,18 @@ class NoteRequester : MviDispatcherKTX<NoteEvent>() {
   override fun input(event: NoteEvent) {
     viewModelScope.launch {
       when (event) {
+        is NoteEvent.MarkItem -> sendResult(event.copy(DataRepository.instance.updateNote(event.note!!)))
+        is NoteEvent.UpdateItem -> sendResult(event.copy(DataRepository.instance.updateNote(event.note!!)))
+        is NoteEvent.AddItem -> sendResult(event.copy(DataRepository.instance.insertNote(event.note!!)))
+        is NoteEvent.RemoveItem -> sendResult(event.copy(DataRepository.instance.deleteNote(event.note!!)))
         is NoteEvent.GetNoteList -> {
           DataRepository.instance.getNotes().collect { sendResult(event.copy(it)) }
-        }
-        is NoteEvent.MarkItem -> {
-          val success = DataRepository.instance.updateNote(event.note!!)
-          if (success) sendResult(event.copy(success))
-        }
-        is NoteEvent.UpdateItem -> {
-          val success = DataRepository.instance.updateNote(event.note!!)
-          if (success) sendResult(event.copy(success))
         }
         is NoteEvent.ToppingItem -> {
           val success = DataRepository.instance.updateNote(event.note!!)
           if (success) DataRepository.instance.getNotes().collect {
             sendResult(NoteEvent.GetNoteList(it))
           }
-        }
-        is NoteEvent.AddItem -> {
-          val success = DataRepository.instance.insertNote(event.note!!)
-          if (success) sendResult(event.copy(success))
-        }
-        is NoteEvent.RemoveItem -> {
-          val success = DataRepository.instance.deleteNote(event.note!!)
-          if (success) sendResult(event.copy(success))
         }
       }
     }
