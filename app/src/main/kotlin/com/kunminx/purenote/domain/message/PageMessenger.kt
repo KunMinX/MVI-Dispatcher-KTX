f4e2a68ffd3d8c6d1ebbec919d36e5add6cc155a
kunminx <kunminx@gmail.com>
2022-07-16 14:36:08 +0800

    update comments

--- app/src/main/kotlin/com/kunminx/purenote/domain/message/PageMessenger.kt
+++ app/src/main/kotlin/com/kunminx/purenote/domain/message/PageMessenger.kt
@@ -14,7 +14,7 @@ class PageMessenger : MviDispatcherKTX<Messages>() {
    *  作为 '唯一可信源'，接收发自页面消息，内部统一处理业务逻辑，并通过 sendResult 结果分发。
    *  ~
    *  与此同时，作为唯一可信源成熟态，
-   *  自动消除 “mutable 样板代码 + LiveData 连发事件覆盖 + LiveData.setValue 误用滥用” 高频痛点。
+   *  自动消除 “mutable 样板代码 + mutableSharedFlow.emit 误用滥用” 高频痛点。
    */
   override fun input(event: Messages) {
     viewModelScope.launch {
@@ -23,7 +23,7 @@ class PageMessenger : MviDispatcherKTX<Messages>() {
 
 // TODO：tip 2：除接收 Activity/Fragment 事件，亦可从 Dispatcher 内部发送事件（作为副作用）：
 //  if (此处欲内部推送) {
-//    Messages msg = new Messages(Messages.EVENT_SHOW_DIALOG);
+//    val msg = Messages(Messages.ShowDialog);
 //    sendResult(msg);
 //  }
   }
--- app/src/main/kotlin/com/kunminx/purenote/domain/request/ComplexRequester.kt
+++ app/src/main/kotlin/com/kunminx/purenote/domain/request/ComplexRequester.kt
@@ -17,14 +17,14 @@ class ComplexRequester : MviDispatcherKTX<ComplexEvent>() {
    *  作为 '唯一可信源'，接收发自页面消息，内部统一处理业务逻辑，并通过 sendResult 结果分发。
    *  ~
    *  与此同时，作为唯一可信源成熟态，
-   *  自动消除 “mutable 样板代码 + LiveData 连发事件覆盖 + LiveData.setValue 误用滥用” 高频痛点。
+   *  自动消除 “mutable 样板代码 + mutableSharedFlow.emit 误用滥用” 高频痛点。
    */
   @SuppressLint("CheckResult")
   override fun input(event: ComplexEvent) {
     viewModelScope.launch {
       when (event) {
         //TODO tip 3: 定长队列，随取随用，绝不丢失事件
-        // 此处通过 RxJava 轮询模拟事件连发，可于 Logcat Debug 见输出
+        // 此处通过 Flow 轮询模拟事件连发，可于 Logcat Debug 见输出
 
         is ComplexEvent.ResultTest1 -> interval().collect {
           val event1 = ComplexEvent.ResultTest4(it)
